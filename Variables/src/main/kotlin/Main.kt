d2609767f2f8275d96b3b7f1caa153bf15f7bf47
Rachel Kenyani <rachelkenyani@gmail.com>
2023-02-02 16:53:27 +0300

    First commit

---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var fullName = "Rachel Kenyani"
+    var age = 20
+    var weightInKg = 65
+    var phoneNumber = "+254710515773"
+    var kenyan: Boolean = true
+}
